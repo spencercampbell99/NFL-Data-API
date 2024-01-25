391082dc5bad9f71a0a5958edeae5d15393b2b4c
spencercampbell99 <spencer.campbell1@maine.edu>
2024-01-25 16:16:22 -0500

    App: Add navbar dropdowns

--- nfl-stats/src/components/nav/navbar.component.tsx
+++ nfl-stats/src/components/nav/navbar.component.tsx
@@ -1,11 +1,77 @@
+'use client'
+
 import Image from 'next/image';
 import NavbarLink from './navbarLink.component';
+import NavbarDropdown from './navbarDropdown.component';
 import React, { FunctionComponent } from 'react';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+interface NavbarDropdowns {
+    [key: string]: {
+        title: string,
+        links: React.ReactNode,
+        active: boolean
+    }
+}
 
 const Navbar: FunctionComponent<{}> = () => {
+    const [dropdowns, setDropdowns] = React.useState<NavbarDropdowns>({
+        games: {
+            title: 'Games',
+            links: (
+                <>
+                    <NavbarLink href="/nfl/games/2023/15">
+                        Week Breakdowns
+                    </NavbarLink>
+                    <NavbarLink href="/nfl/games/2023/15/model_performance">
+                        Weekly Model Performance
+                    </NavbarLink>
+                </>
+            ),
+            active: false
+        },
+        players: {
+            title: 'Players',
+            links: (
+                <>
+                    <NavbarLink href="/nfl/players/game/">
+                        Game Breakdowns
+                    </NavbarLink>
+                    <NavbarLink href="/nfl/players">
+                        Player Overviews
+                    </NavbarLink>
+                </>
+            ),
+            active: false
+        },
+    })
+
+    // toggle the dropdown menu
+    const toggleActiveDropdown = (dropdown: string) => {
+        setDropdowns(prevState => ({
+            ...prevState,
+            [dropdown]: {
+                ...prevState[dropdown],
+                active: !prevState[dropdown].active
+            }
+        }));
+    };
+
+    // close all dropdowns when route has changed
+    const pathname = usePathname();
+    React.useEffect(() => {
+        setDropdowns(prevState => {
+            const newState = { ...prevState };
+            for (const dropdown in newState) {
+                newState[dropdown].active = false;
+            }
+            return newState;
+        });
+    }, [pathname]);
+
     return (
-        <nav className="bg-gradient-to-b from-mediumGreen to-darkGreen h-[75px] max-h-[75px]">
+        <nav className="bg-gradient-to-b from-mediumGreen to-darkGreen h-[75px] max-h-[75px] align-middle">
             <Link href="/"><Image src="/rectangle-logo.png" alt="Logo" width={125} height={100} className="absolute pl-3 pt-[6px]" /></Link>
             <ul className="flex justify-center space-x-4 py-6">
                 <li>
@@ -14,14 +80,14 @@ const Navbar: FunctionComponent<{}> = () => {
                     </NavbarLink>
                 </li>
                 <li>
-                    <NavbarLink href="/nfl/games/2023/15">
-                        Current Week
-                    </NavbarLink>
+                    <NavbarDropdown title={dropdowns.games.title} isActive={dropdowns.games.active} toggleActive={() => toggleActiveDropdown('games')}>
+                        {dropdowns.games.links}
+                    </NavbarDropdown>
                 </li>
                 <li>
-                    <NavbarLink href="/nfl/games/2023/15/model_performance">
-                        Model Performance
-                    </NavbarLink>
+                    <NavbarDropdown title={dropdowns.players.title} isActive={dropdowns.players.active} toggleActive={() => toggleActiveDropdown('players')}>
+                        {dropdowns.players.links}
+                    </NavbarDropdown>
                 </li>
             </ul>
         </nav>
---
+++ nfl-stats/src/components/nav/navbarDropdown.component.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+type NavbarDropdownProps = {
+    title: string;
+    isActive: boolean;
+    toggleActive: any;
+    children?: React.ReactNode;
+};
+
+const NavbarDropdown: React.FunctionComponent<NavbarDropdownProps> = ({ title, isActive, toggleActive, children }) => {
+    return (
+        <>
+            <div className="relative inline-block text-left">
+                <div>
+                    <button type="button" onClick={toggleActive} className="inline-flex justify-center w-auto rounded-md border border-gray-300 shadow-sm px-4 py-1 bg-mediumGreen text-sm font-medium text-offWhite hover:bg-darkGreen">
+                        {title}
+                    </button>
+                </div>
+
+                {isActive ? (
+                    <div className="absolute left-1/2 -translate-x-1/2 mt-2 w-56 rounded-md shadow-lg bg-nflGreen ring-1 ring-black ring-opacity-5">
+                        <div className="p-1 text-center" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
+                            {children}
+                        </div>
+                    </div>
+                ) : null}
+            </div>
+        </>
+    )
+}
+
+export default NavbarDropdown;
