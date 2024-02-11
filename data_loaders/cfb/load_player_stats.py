7a0baa15fdb3484c563f3e87d475e6d5e5e4b7dc
spencercampbell99 <sallertonc@gmail.com>
2024-02-11 09:49:57 -0500

    Loaders: WIP load player stats

--- data_loaders/cfb/load_player_stats.py
+++ data_loaders/cfb/load_player_stats.py
@@ -1,6 +1,6 @@
 import json
 import pandas as pd
-from SQLConnector import MySQLConnection
+from CfbSQLConnector import MySQLConnection
 from CollegeFootballDataApi import APIClient
 from sqlalchemy import text
 
@@ -11,13 +11,57 @@ except FileNotFoundError:
     print("Error: cfb_api_key.txt file not found. Create the file in /cfb with your API key and try again.")
     exit()
 
-data = api_client.call_endpoint('games/players', params={'year': 2023, 'week': 1, 'seasonType': 'regular'}, verbose=True)
-
-# write to example_player_data.json
-with open('cfb/example_data/example_player_data.json', 'w') as file:
-    file.write(json.dumps(data, indent=4))
+data = api_client.call_endpoint('games/players', params={'year': 2023, 'week': 1, 'seasonType': 'regular', 'gameId': 401540200}, verbose=True)
 
 # convert the list of teams to a DataFrame
 data = pd.DataFrame(data)
+                
+stat_categories = [
+    'punting',
+    'kicking',
+    'puntReturns',
+    'kickReturns',
+    'interceptions',
+    'fumbles',
+    'receving',
+    'rushing',
+    'passing',
+]
 
-print(data.columns)
+# stat name conversions
+stat_name_conversion = {
+    'LONG': 'long',
+    'In 20': 'inside_20',
+    'TB': 'touchbacks',
+    'AVG': 'avg',
+    'YDS': 'yards',
+    'NO': 'kicks',
+    "PTS": "points",
+    "XP": "extra_points",
+    "PCT": "pct",
+    "FG": "field_goals",
+    "TD": "touchdowns",
+    "INT": "interceptions",
+    "LOST": "fumbles_lost",
+    "FUM": "fumbles",
+    "CAR": "carries",
+    "REC": "receptions",
+    "C/ATT": ["completions", "attempts"], # define in loop
+}
+
+def convert_stat_name(stat_name):
+    if stat_name in stat_name_conversion:
+        return stat_name_conversion[stat_name]
+    else:
+        return stat_name
+    
+def load_passing_stats(data):
+    """
+    Load passing stats into the database.
+    
+    Args:
+        data (pd.DataFrame): The DataFrame containing the passing stats.
+    """
+    
+    # insert query
+    insert_query = text("INSERT INTO player_game_passing_stats (game_id, player_id, completions, attempts, yards, touchdowns, interceptions, average_per_attempt, average_per_completion) VALUES (:game_id, :player_id, :completions, :attempts, :yards, :touchdowns, :interceptions, :average_per_attempt, :average_per_completion)")
